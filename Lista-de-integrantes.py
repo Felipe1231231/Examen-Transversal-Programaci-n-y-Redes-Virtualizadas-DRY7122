0e04a286877c28ecd27084c8c16c6aa11064e8b2
NetAcad DEVASC <changeme@example.com>
2023-07-06 15:34:23 +0000

    EVALUACION ET

---
+++ Lista-de-integrantes.py
@@ -0,0 +1,9 @@
+lista = ["FELIPE MANRIQUE LOPEZ", "ELIAS APABLAZA CASTILLO", "ANGEL SAAVEDRA MACHUCA", "MATIAS DONATO ELGUETA"]
+
+print("El primer integrante es: " + lista[0])
+
+print("El segundo integrante es: " + lista[1])
+
+print("El tercer integrante es: " + lista[2])
+
+print("El cuarto integrante es: " + lista[3])
